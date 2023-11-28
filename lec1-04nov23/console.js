c9ac238f27aabe46a2555c0adc2bced98c8f6871
rishikaa17 <rishikamishra581@gmail.com>
2023-11-28 23:34:46 +0530

    first commit

---
+++ lec1-04nov23/console.js
@@ -0,0 +1,3 @@
+console.log("Rishika R Mishra")
+console.error("Rishika R Mishra")
+console.warn("Rishika R Mishra")
---
+++ lec1-04nov23/variable.js
@@ -0,0 +1,15 @@
+//variable : let 
+//constant : const
+
+
+//email = "rishikamishra581gmail.com"
+let email = "rishikamishra581@gmail.com"
+const email1 = "rishikamishra581@gmail.com"
+console.log(email);
+const firstname = "Rishika Mishra"
+const age = 22
+let loggedin = false
+const nodeversion = null
+let bankballance = undefined
+const buttonId = Symbol("id")
+const areallyLongNumber = 9848486597107795329n
