abfc98a6d223e82465db9bc296050be680e308a3
rishikaa17 <rishikamishra581@gmail.com>
2023-11-29 18:41:48 +0530

    modified

--- lec1-04nov23/variable.js
+++ lec1-04nov23/variable.js
@@ -3,6 +3,7 @@
 
 
 //email = "rishikamishra581gmail.com"
+//examples  of variable  
 let email = "rishikamishra581@gmail.com"
 const email1 = "rishikamishra581@gmail.com"
 console.log(email);
@@ -13,3 +14,4 @@ const nodeversion = null
 let bankballance = undefined
 const buttonId = Symbol("id")
 const areallyLongNumber = 9848486597107795329n
+
