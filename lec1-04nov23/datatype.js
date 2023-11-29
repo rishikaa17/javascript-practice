6d8be242dab54e12af852137cece984bcbf304be
rishikaa17 <rishikamishra581@gmail.com>
2023-11-29 18:33:36 +0530

    datatypes learning

---
+++ lec1-04nov23/datatype.js
@@ -0,0 +1,33 @@
+//premitive datatype (STACK)
+
+// String 
+// Number 
+// boolian
+// Null
+// undefined
+// Symbol
+// BigInt
+
+//non primitive datatype -object (HEAP)
+
+/*
+Array 
+objects
+functiom
+*/
+
+const numbers = [1,2,3,4,5,6,7,8,9,]
+const heros = ["superman","Batman","Flash"]
+
+const userObjects = { 
+    name: "Rishiaka",
+    age: 22,
+    isLoggedIn: true,
+}
+
+ const sayHellow = function (){
+    console.logJ("Rishika R Midhra")
+ }
+
+ const registerAUser = function(){}
+ const feature = functionL(){}
