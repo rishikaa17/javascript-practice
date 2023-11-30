ddd9c6408909c13631baf7559600d55580b667de
rishikaa17 <rishikamishra581@gmail.com>
2023-11-30 11:36:01 +0530

    first commit

--- lec2-5nov23/conversion.js
+++ lec2-5nov23/conversion.js
@@ -1,1 +1,9 @@
-hello
+let bankBalance = '-500'
+let bankbalanceInt = parseFloat(bankBalance) //or let bankbalanceInt = Number(bankBalance)
+console.log(bankBalance)
+console.log(bankbalanceInt)
+console.log(typeof bankBalance)
+console.log(typeof bankbalanceInt)
+
+console.log(5 + undefined)
+console.log(5 + null)
---
+++ lec2-5nov23/numAndString.js
@@ -0,0 +1,19 @@
+/*
+
+const firstname = "Rishika"
+const lastname  ='Mishra'
+console.log(firstname + ' '+ lastname )
+console.log (`My first name is ${firstname} and my last name is ${lastname}` )
+const username = new String("RRM")
+console.log(username[0]);
+console.log(username.length);
+console.log(username.charAt(0));
+console.log(username.substring(0,3));
+let email = "    rishi@gmail.com   "
+console.log(email.trim())
+let emailtwo = "rrm.com"
+console.log(emailtwo.replace("rrm","rishika"))
+console.log(email.includes('rishi'))
+console.log(email.toUpperCase())
+
+*/
